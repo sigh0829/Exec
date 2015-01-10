532c3dc5a45016fe1d90ddd6ae57ffe7ba0ad3e7
KevinPas <kevinpas@hushmail.com>
2015-01-10 10:20:31 -0700

    Added new rest api client and server side handlers.

---
+++ Live/Sites/WinJS_3/EXEC-INF/SiteApps/Rest/myApi_c.js
@@ -0,0 +1,344 @@
+//	The MIT License (MIT) 
+//	
+//	
+//	Copyright (c) 2014 Kevin R. Pastorino 
+//	 
+//	
+//	Permission is hereby granted, free of charge, to any person obtaining a copy 
+//	of this software and associated documentation files (the "Software"), to deal 
+//	in the Software without restriction, including without limitation the rights 
+//	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+//	copies of the Software, and to permit persons to whom the Software is 
+//	furnished to do so, subject to the following conditions: 
+//	 
+//	
+//	The above copyright notice and this permission notice shall be included in all 
+//	copies or substantial portions of the Software. 
+//	 
+//	
+//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+//	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+//	SOFTWARE. 
+
+
+if ( typeof window !== "undefined" )
+	jsGlobal = window;
+else
+{
+	//	In vertx global hasn't been defined yet inside of a module.
+	if ( typeof global !== "undefined" )
+		jsGlobal = global;
+	else
+	{
+		//	execGlobal.js is empty.  It's only purpose is to
+		//	create a global variable in vertx.  The first
+		//	time vertx "requires" a file it sets up an internal
+		//	global so that the next time a user tries to load
+		//	the file it will check to see if that has already been
+		//	done and if so it will not read it again but instead
+		//	return a reference to the already created global.
+		jsGlobal = require( './vertxGlobal.js' );
+	}
+}
+
+
+if ( typeof jsGlobal.site_winjs_3 === "undefined" )
+	jsGlobal.site_winjs_3 = {};
+
+if ( typeof jsGlobal.site_winjs_3.myapi === "undefined" )
+	jsGlobal.site_winjs_3.myapi = {};
+
+
+//	Class MyApi_c start
+//
+
+(function( namespace )
+{
+	function MyApi_c ()
+	{
+        try
+        {
+            //  Windows APIs:   http://msdn.microsoft.com/en-us/library/windows/apps/br211377.aspx
+            //  WinJS.xhr:      http://msdn.microsoft.com/en-us/library/windows/apps/br229787.aspx
+            //  startLog:       http://msdn.microsoft.com/en-us/library/windows/apps/hh701617.aspx
+            //  stopLog:        http://msdn.microsoft.com/en-us/library/windows/apps/hh701626.aspx
+            //  winJS.log:      http://msdn.microsoft.com/en-us/library/windows/apps/jj150612.aspx
+
+            
+            //  log type can be: error, warn, info, perf
+            WinJS.Utilities.startLog( {type: "info", tags: "winjs" } );
+            WinJS.log( "MyApi_c, starting", "winjs", "info" );
+            //WinJS.Utilities.stopLog();
+
+            
+            //  Four of the possible six message types: text, json, blob, arraybuffer
+
+
+            //  Get some text from the website at "hostname" and "port".
+            //  Get it from the rest handler that services "namespace.MyApi_c.restApi"
+            this.restRequest    =   location.protocol + "//" + location.hostname + ":" + location.port;
+            this.restRequest    +=  "/" + namespace.MyApi_c.restApi + "?name=fred&age=33&returnType=text";
+            WinJS.log( "MyApi_c, restRequest = " + this.restRequest, "winjs", "info" );
+            WinJS.xhr
+            ({ url: this.restRequest, responseType: "text" }
+            ).done( this.textCompleted, this.textError, this.textProgress );
+
+
+            //  Get some json.
+		    this.restRequest    =   location.protocol + "//" + location.hostname + ":" + location.port;
+            this.restRequest    +=  "/" + namespace.MyApi_c.restApi + "?name=fred&age=33&returnType=json";
+            WinJS.log( "MyApi_c, restRequest = " + this.restRequest, "winjs", "info" );
+            WinJS.xhr
+            ({ url: this.restRequest, responseType: "text" }
+            ).done( this.jsonCompleted, this.jsonError, this.jsonProgress );
+
+
+            //  Get a file.
+		    this.restRequest    =   location.protocol + "//" + location.hostname + ":" + location.port;
+            this.restRequest    +=  "/" + namespace.MyApi_c.restApi + "?name=fred&age=33&returnType=blob";
+            WinJS.log( "MyApi_c, restRequest = " + this.restRequest, "winjs", "info" );
+            WinJS.xhr
+            ({ url: this.restRequest, responseType: "blob" }
+            ).done( this.blobCompleted, this.blobError, this.blobProgress );
+
+
+            //  Get some binary data in ArrayBuffer format.
+            //  Warning:  this section will not work with vertx 2.0 because
+            //  vertx does not provide ArrayBuffer support.  So, you
+            //  will see an exception.
+		    this.restRequest    =   location.protocol + "//" + location.hostname + ":" + location.port;
+            this.restRequest    +=  "/" + namespace.MyApi_c.restApi + "?name=fred&age=33&returnType=arrayBuffer";
+            WinJS.log( "MyApi_c, restRequest = " + this.restRequest, "winjs", "info" );
+            WinJS.xhr
+            ({ url: this.restRequest, responseType: "arraybuffer" }
+            ).done( this.arrayBufferCompleted, this.arrayBufferError, this.arrayBufferProgress );
+        }
+
+        catch ( err )
+        {
+            console.log ( "index.html, catch err = " + err );
+        }
+	};
+	
+	MyApi_c.prototype.textCompleted	=function ( result )    {
+
+        WinJS.log( "myApi_c.js, WinJS.xhr, textCompleted, result.status = " + result.status,  "winjs", "info" );
+
+        if ( result.status === 0 )
+        {
+            WinJS.log( "Failure",  "winjs", "info" );
+            WinJS.log( "myApi_c.js, WinJS.xhr, textCompleted, You may need to change the http port ",  "winjs", "info" );
+        }
+                    
+        else if ( result.status === 200 )
+        {
+            try
+            {
+                WinJS.log( "myApi_c.js, WinJS.xhr, textCompleted, result.responseText = " + result.responseText,  "winjs", "info" );
+
+                //  Put the text that we got from the server into "<div id='divResult'>"
+                var divResult  = document.getElementById( "divResult" );
+                    divResult  .innerText               = result.responseText;  //  ie
+                    divResult  .textContent             = result.responseText;  //  firefox
+                    divResult  .style.backgroundColor   = "lightGreen";
+
+                //WinJS.log( "myApi_c.js, WinJS.xhr, textCompleted, divResult = " + divResult.innerText,  "winjs", "info" );
+            }
+            catch ( err )
+            {
+                console.log ( "myApi_c.js, WinJS.xhr, textCompleted, catch err = " + err );
+            }
+        }
+	};
+	
+	MyApi_c.prototype.textError = function( result ) {
+
+        WinJS.log( "myApi_c.js, WinJS.xhr, textError",  "winjs", "info" );
+	};
+	
+	MyApi_c.prototype.textProgress= function( result ) {
+
+        WinJS.log( "myApi_c.js, WinJS.xhr, textProgress",  "winjs", "info" );
+	};
+	
+	
+    MyApi_c.prototype.jsonCompleted	=function ( result )    {
+
+        WinJS.log( "myApi_c.js, WinJS.xhr, jsonCompleted, result.status = " + result.status,  "winjs", "info" );
+
+        if ( result.status === 0 )
+        {
+            WinJS.log( "Failure",  "winjs", "info" );
+            WinJS.log( "myApi_c.js, WinJS.xhr, jsonCompleted, You may need to change the http port ",  "winjs", "info" );
+        }
+                    
+        else if ( result.status === 200 )
+        {
+            try
+            {
+                WinJS.log( "myApi_c.js, WinJS.xhr, jsonCompleted, result.responseText = " + result.responseText,  "winjs", "info" );
+            }
+            catch ( err )
+            {
+                console.log ( "myApi_c.js, WinJS.xhr, jsonCompleted, catch err = " + err );
+            }
+        }
+	};
+	
+	MyApi_c.prototype.jsonError = function( result ) {
+
+        WinJS.log( "myApi_c.js, WinJS.xhr, jsonError",  "winjs", "info" );
+	};
+	
+	MyApi_c.prototype.jsonProgress= function( result ) {
+
+        WinJS.log( "myApi_c.js, WinJS.xhr, jsonProgress",  "winjs", "info" );
+	};
+	
+	
+	MyApi_c.prototype.blobCompleted	=function ( result )    {
+
+        WinJS.log( "myApi_c.js, WinJS.xhr, blobCompleted, result.status = " + result.status,  "winjs", "info" );
+
+        if ( result.status === 0 )
+        {
+            WinJS.log( "Failure",  "winjs", "info" );
+            WinJS.log( "myApi_c.js, WinJS.xhr, blobCompleted, You may need to change the http port ",  "winjs", "info" );
+        }
+                    
+        else if ( result.status === 200 )
+        {
+            try
+            {
+                if( result.response instanceof Blob )
+                {
+                    WinJS.log( "myApi_c.js, WinJS.xhr, blobCompleted",  "winjs", "info" );
+
+                    //WinJS.log( "myApi_c.js, WinJS.xhr, blobCompleted, Blob = "      + result.response,      "winjs", "info" );
+                    //WinJS.log( "myApi_c.js, WinJS.xhr, blobCompleted, Blob.type = " + result.response.type, "winjs", "info" );
+
+                    var divResult   = document.getElementById( "divResult" );
+                        divResult   .style.backgroundColor = "lightGreen";
+
+                    //result.response .type = 'image/gif';
+                    //WinJS.log( "myApi_c.js, WinJS.xhr, blobCompleted, Blob.type = " + result.response.type, "winjs", "info" );
+
+                    //  http://stackoverflow.com/questions/19672685/setting-binary-data-on-img
+                    var img = document.createElement("IMG");
+                        img .alt = "alt alt alt";
+                        img .src = URL.createObjectURL( result.response );
+
+                    divResult.appendChild( img );
+                }
+            }
+            catch ( err )
+            {
+                console.log ( "myApi_c.js, WinJS.xhr, blobCompleted, catch err = " + err );
+            }
+        }
+	};
+	
+	MyApi_c.prototype.blobError = function( result ) {
+
+        WinJS.log( "myApi_c.js, WinJS.xhr, blobError",  "winjs", "info" );
+	};
+	
+	MyApi_c.prototype.blobProgress= function( result ) {
+
+        WinJS.log( "myApi_c.js, WinJS.xhr, blobProgress",  "winjs", "info" );
+	};
+	
+	
+	MyApi_c.prototype.arrayBufferCompleted	=function ( result )    {
+
+        WinJS.log( "myApi_c.js, WinJS.xhr, arrayBufferCompleted, result.status = " + result.status,  "winjs", "info" );
+
+        if ( result.status === 0 )
+        {
+            WinJS.log( "Failure",  "winjs", "info" );
+            WinJS.log( "myApi_c.js, WinJS.xhr, arrayBufferCompleted, You may need to change the http port ",  "winjs", "info" );
+        }
+                    
+        else if ( result.status === 200 )
+        {
+            try
+            {
+                WinJS.log( "myApi_c.js, WinJS.xhr, arrayBufferCompleted, 1",  "winjs", "info" );
+
+                if( result.response instanceof ArrayBuffer )
+                {
+                    WinJS.log( "myApi_c.js, WinJS.xhr, arrayBufferCompleted, 2 = " + result.response,  "winjs", "info" );
+
+                    //  Warning:  this section will not work with vertx 2.0 because
+                    //  vertx does not provide ArrayBuffer support.  So, you
+                    //  will see an exception.
+
+                    //  http://chimera.labs.oreilly.com/books/1230000000545/ch17.html#_receiving_text_and_binary_data
+                    var usernameView    = new Uint8Array    ( result.response,  0,  16  );
+                    var idView          = new Uint16Array   ( result.response,  16, 1   );
+                    var scoresView      = new Float32Array  ( result.response,  20, 32  );
+
+                    usernameView        = jsGlobal.exec.any.utils.AnyUtils.Utf8ArrayToStr         ( usernameView );
+
+                    WinJS.log( "myApi_c.js, WinJS.xhr, arrayBufferCompleted, usernameView = "   + usernameView,     "winjs", "info" );
+                    WinJS.log( "myApi_c.js, WinJS.xhr, arrayBufferCompleted, idView = "         + idView[ 0 ],      "winjs", "info" );
+
+                    for ( var j = 0; j < 32; j++ )
+                        WinJS.log( "myApi_c.js, WinJS.xhr, arrayBufferCompleted, scoresView = " + scoresView[ j ],  "winjs", "info" );
+                }
+            }
+            catch ( err )
+            {
+                console.log ( "myApi_c.js, WinJS.xhr, arrayBufferCompleted, catch err = " + err );
+            }
+        }
+	};
+	
+	MyApi_c.prototype.arrayBufferError = function( result ) {
+
+        WinJS.log( "myApi_c.js, WinJS.xhr, arrayBufferError",  "winjs", "info" );
+	};
+	
+	MyApi_c.prototype.arrayBufferProgress= function( result ) {
+
+        WinJS.log( "myApi_c.js, WinJS.xhr, arrayBufferProgress",  "winjs", "info" );
+	};
+
+
+    namespace.MyApi_c	= MyApi_c;
+	
+	//	Static variables:
+	//	{
+			//	There may be many classes in this name space so include 
+			//	the class name where these statics are located.
+			namespace.MyApi_c	.restApi    = "myApi";
+	//	}
+
+}( jsGlobal.site_winjs_3.myapi ) );	//	Attach to this namespace
+
+if ( typeof module !== "undefined"  &&  module !== null  &&  typeof module === "object" )
+{
+	//console.log ( "site_winjs_3.underscore module = " + module );
+	
+	if ( module.exports !== null  &&  typeof module.exports === "object" )
+	{
+		//console.log ( "site_winjs_3.underscore module.exports = " + module.exports );
+
+		module.exports	= jsGlobal.site_winjs_3.myapi;
+		//module.exports.MyApi_c	= site_winjs_3.underscore.MyApi_c;
+		//console.log ( "site_winjs_3.underscore module.exports.MyApi_c = " + module.exports.MyApi_c );
+
+
+		//module.exports.MyApi_c._	= site_winjs_3.underscore.MyApi_c;
+		//console.log ( "site_winjs_3.underscore module.exports.MyApi_c = " + module.exports.MyApi_c );
+	}
+}
+
+
+//
+//	Class MyApi_c end
+
---
+++ Live/Sites/WinJS_3/EXEC-INF/SiteApps/Rest/myApi_s.js
@@ -0,0 +1,234 @@
+//	The MIT License (MIT) 
+//	
+//	
+//	Copyright (c) 2014 Kevin R. Pastorino 
+//	 
+//	
+//	Permission is hereby granted, free of charge, to any person obtaining a copy 
+//	of this software and associated documentation files (the "Software"), to deal 
+//	in the Software without restriction, including without limitation the rights 
+//	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+//	copies of the Software, and to permit persons to whom the Software is 
+//	furnished to do so, subject to the following conditions: 
+//	 
+//	
+//	The above copyright notice and this permission notice shall be included in all 
+//	copies or substantial portions of the Software. 
+//	 
+//	
+//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+//	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+//	SOFTWARE. 
+
+//  http://localhost:7777/myApi?prop&value;
+//  http://localhost:7777/myApi?name=fred&age=33
+
+//  Cannot change this to lower case because
+//  http://localhost:7777/myApi  is different than
+//  http://localhost:7777/myapi
+
+//  Use myApi_c.js for both the client and server side.
+var myApi_c = require( './myApi_c.js' );
+
+module.exports = function ()	{
+
+    var luo 			= {};	//	Local Use Only
+        luo .message    = "";
+    	luo .system     = null;
+
+        luo .AnyUtils   = null;
+        luo .console    = null;
+        luo .fileImp    = null;
+        luo .httpImp    = null;
+        luo .site       = null;
+        luo .Version    = null;
+
+	this.execute = function ( params )	{
+
+        var jsonResult  = {};
+
+        try
+        {
+            //  Get the system support objects
+            if (    luo.system === null                     &&  
+                    typeof params.system !== "undefined"    &&  
+                    params.system !== null                  &&  
+                    typeof params.system.execute === "function"
+               )
+            {
+    	        luo.system  = params.system;
+
+                luo.AnyUtils    = luo.system.execute ({ "get": "AnyUtils",  "returnIn": "AnyUtils", "defaultValue": null }).AnyUtils;
+                luo.console     = luo.system.execute ({ "get": "console",   "returnIn": "console",  "defaultValue": null }).console;
+                luo.fileImp     = luo.system.execute ({ "get": "fileImp",   "returnIn": "fileImp",  "defaultValue": null }).fileImp;
+                luo.httpImp     = luo.system.execute ({ "get": "httpImp",   "returnIn": "httpImp",  "defaultValue": null }).httpImp;
+                luo.site        = luo.system.execute ({ "get": "site",      "returnIn": "site",     "defaultValue": null }).site;
+                luo.Version     = luo.system.execute ({ "get": "Version",   "returnIn": "Version",  "defaultValue": null }).Version;
+            }
+
+            //  All execute functions are told by the caller
+            //  where to put the return value.  This is the name
+            //  of the property in the json object where the caller
+            //  will look for the result.  For example if the user
+            //  wants the result in a property called "pathname" they
+            //  would set up execute() like this:
+            //  var	result      = httpImp.execute( { "system":luo.system, "job": "doSomething"  "returnIn": "pathname", "defaultValue": "myERROR", "vt":"krp", "v": "1.0.0" } );
+            //  var pathname    = result.pathname;
+            //  if ( pathname === "myERROR" ) {}
+            jsonResult  [ params.returnIn ] = params.defaultValue;
+
+            //luo.console.log( "myApi, execute, 1 = " );
+
+            if ( luo.Version.versionOK( params.v, 1, 0, 0 ) === true )
+            {
+                //luo.console.log( "myApi, execute, 2 = " );
+                jsonResult[ params.returnIn ] = luo._execute ( params.session, params.methodType, params.method, params.httpStatus );
+            }
+            else
+            {
+                //luo.console.log( "myApi, execute, 3 = " );
+                jsonResult  [ params.returnIn ] = params.defaultValue;
+                luo .message                    = params.v + " is not handled by this implementation";
+            }
+        }
+
+        catch ( err )
+        {
+            luo.console.log( "myApi, execute, catch err = " + err );
+            jsonResult  [ params.returnIn ] = params.defaultValue;
+        }
+
+        //luo.console.log( "myApi, execute, result = " + jsonResult[ params.returnIn ] );
+        return jsonResult;
+    }
+
+    luo._execute = function ( session, methodType, method, httpStatus )  {
+
+        var result = false; //
+
+        method  = method.toString ();
+            
+        if ( method === methodType.NAME )
+        {
+            result = myApi_c.MyApi_c	.restApi;
+        }
+
+        else if ( method === methodType.DELETE )
+        {
+            //  
+        }
+
+        else if ( method === methodType.GET )
+        {
+            //  http://localhost:7777/myApi?name=fred&age=33
+
+		    var parsedQuery	= luo.httpImp.execute( { "system":luo.system, "session": session, "job": "getRequestQuery", "returnIn": "parsedQuery", "defaultValue": "ERROR", "vt":"krp", "v": "1.0.0" } ).parsedQuery;
+		    if ( parsedQuery !== "ERROR" )
+		    {
+			    //luo.console.log( "myApi.GET, parsedQuery = "  + parsedQuery );
+			    //luo.console.log( "myApi.GET, query.name = "	+ parsedQuery.name );
+			    //luo.console.log( "myApi.GET, query.age = " 	+ parsedQuery.age );
+
+                var message = "name is " + parsedQuery.name + ", age is " + parsedQuery.age;
+
+                if ( parsedQuery.returnType === "json" )
+                    message = JSON.stringify( { "name":parsedQuery.name, "age":parsedQuery.age  } );
+
+                else if ( parsedQuery.returnType === "blob" )
+                {
+                    //message = JSON.stringify( { "result": message } );
+
+	                //  Get any image and send it.
+                    var	pathname	= "./" + luo.site + "/PUB-INF/images/" + "pdf.png";
+                        message     = luo.fileImp  .execute	
+                                        ( { "system":this.system, "job":"readBinaryFile", "pathname":pathname, 
+                                                "async":false, "data":"krp", "returnIn": "result", 
+                                                    "defaultValue": { "contents":"" }, "vt":"krp", "v": "1.0.0"  } ).result.contents;
+
+                    //luo.console.log( "myApi, _execute, pathname = " + pathname );
+                }
+
+                else if ( parsedQuery.returnType === "arrayBuffer" )
+                {
+                    //  vertx as of 10/27/2014 does not implement these:
+                    //  ArrayBuffer, Uint16Array, Float32Array
+                    try
+                    {
+                        //  http://chimera.labs.oreilly.com/books/1230000000545/ch17.html#_receiving_text_and_binary_data
+                        //  An ArrayBuffer is a generic, fixed-length binary data buffer.
+                        //  However, an ArrayBuffer can be used to create one or more
+                        //  ArrayBufferView objects, each of which can present the 
+                        //  contents of the buffer in a specific format. For example, 
+                        //  let's assume we have the following C-like binary data structure:
+                        //
+                        //  struct someStruct {
+                        //      char            username[ 16 ];
+                        //      unsigned short  id;
+                        //      float           scores[ 32 ];
+                        //  };
+
+
+                        //  http://chimera.labs.oreilly.com/books/1230000000545/ch17.html#_receiving_text_and_binary_data
+                        //
+                        //  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Typed_arrays?redirectlocale=en-US&redirectslug=JavaScript%2FTyped_arrays
+                        //  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Uint8Array
+
+
+                        //  Create a workarea for the output with the following fields:
+                        //  Uint8Array: 1 byte, Uint16Array: 2 bytes, Float32Array: 4 bytes
+                        message = new ArrayBuffer( 16 + 2 + (4 * 32) + 2 );  
+
+                        //  http://stackoverflow.com/questions/7372124/why-is-creating-a-float32array-with-an-offset-that-isnt-a-multiple-of-the-eleme
+                        //  When using Float32Array the interpreter wants to put the data on
+                        //  a "divide by 4" boundary.  So, use 20;  Otherwise you'll get an exception
+
+                        //  Send the following letters: "01234567890abcdef".
+                        var usernameView    = luo.AnyUtils.str2Uint8Array   ( message,  0,  16, "01234567890abcdef" ); 
+                        var idView          = new Uint16Array               ( message,  16, 1   );
+                        var scoresView      = new Float32Array              ( message,  20, 32  );
+
+                        //  Send the number 65535.
+                        idView [ 0 ] = 65535;
+
+                        //  Send some floating point data.
+                        for ( var i = 0; i < 32; i++ )
+                            scoresView[ i ] = i * 3.14;
+
+                        //  Convert from arrayBuffer to Buffer for the xhr interface.
+                        message = luo.AnyUtils.arrayBufferToBuffer ( message );
+                    }
+
+                    catch ( err )
+                    {
+                        luo.console.log( "myApi, _execute, parsedQuery.returnType === \"arrayBuffer\", catch err = " + err );
+                    }
+                }
+
+                //  Now send it.
+			    luo.httpImp .writeHead  ( session, httpStatus.OK.code );
+		        luo.httpImp .execute    ( { "system":luo.system, "session": session, "job": "end", 
+                                                "data": { "vt":"krp", "v": "1.0.0", "message": message }, 
+                                                    "returnIn": "void", "defaultValue": "void", "vt":"krp", "v": "1.0.0" } );
+
+                result = httpStatus.OK.code;
+		    }
+        }
+            
+        else if ( method === methodType.POST )
+        {
+            //  
+        }
+
+        else if ( method === methodType.PUT )
+        {
+        }
+
+        //luo.console.log( "myApi, _execute, return = " + result );
+
+        return  result
+    }
+};
